57ff95e0955e00ee02b298c44d6786fa78b280ce
ArbSoftware <satabhishar@arbsoft.comm>
2025-07-24 12:21:45 +0530

    modified checkin policy

--- app/Http/Controllers/CheckinPolicyApiController.php
+++ app/Http/Controllers/CheckinPolicyApiController.php
@@ -14,7 +14,6 @@ class CheckinPolicyApiController extends Controller
             'puid' => 'required|string',
             'corp_id' => 'required|string',
             'policy_name' => 'required|string',
-            // ...add other required fields as needed
         ]);
 
         $exists = CheckinPolicy::where('corp_id', $request->corp_id)
@@ -28,7 +27,50 @@ class CheckinPolicyApiController extends Controller
             ], 409);
         }
 
-        $policy = CheckinPolicy::create($request->all());
+        $data = $request->all();
+
+        // Fields that should default to "N/A" if empty
+        $fieldsToDefaultNA = [
+            'policy_name',
+            'applicability_type',
+            'applicability_for',
+            'advnc_applicability_type',
+            'advnc_applicability_for'
+        ];
+
+        foreach ($fieldsToDefaultNA as $field) {
+            if (!isset($data[$field]) || $data[$field] === null || $data[$field] === '') {
+                $data[$field] = 'N/A';
+            }
+        }
+
+        // All other fields (except timestamps and id) default to 0 if empty
+        $fieldsToDefaultZero = [
+            'web_checkin_yn', 'punches_yn', 'punches_no', 'restrict_emp_marking_attndc_yn', 'punch_start_time', 'punch_end_time',
+            'IP_validation_yn', 'from_ip', 'to_ip', 'web_chckin_rqst_frm_whatsapp_yn', 'web_chckin_rqst_frm_teams_yn',
+            'mobile_chckin_yn', 'photo_attdnc_yn', 'no_of_photos', 'location_attdnc_yn', 'adv_location_tracking_yn',
+            'specific_period_wrk_location_approval_yn', 'location_approval_days_limit', 'max_punches_allowed_yn',
+            'punches_no_allowed', 'restrict_emp_attndc_yn', 'attdnc_regularization_yn', 'emp_bck_dated_attdnc_regularization_yn',
+            'emp_bck_dated_attdnc_regularization_days', 'mngr_bck_dated_regularization_yn', 'mngr_bck_dated_regularization_days',
+            'hr_bck_dated_attdnc_regularization_yn', 'hr_bck_dated_attdnc_regularization_days', 'attdnc_regularization_limit_type',
+            'attdnc_regularization_total_limit', 'ar_aftr_attdnc_process_yn', 'future_dtd_attdnc_regularization_yn',
+            'atleast_one_punch_attdnc_regularization_yn', 'for_ar_attachment_yn', 'whatsapp_ar_rqst_yn', 'teams_ar_rqst_yn',
+            'ar_week_off_emp_restricted_yn', 'ar_holidays_emp_restricted_yn', 'on_duty_yn', 'emp_bck_dtd_onduty_rqst_yn',
+            'emp_bck_dtd_onduty_rqst_days', 'mngr_bck_dtd_onduty_rqst_yn', 'mngr_bck_dtd_onduty_rqst_days',
+            'hr_bck_dtd_onduty_rqst_yn', 'hr_bck_dtd_onduty_rqst_days', 'configure_overall_onduty_limit_yn',
+            'project_log_time_yn', 'raise_onduty_aftr_attdnc_process_yn', 'future_dtd_onduty_yn',
+            'restrict_manager_onduty_beyond_limit_yn', 'restrict_hr_onduty_beyond_limit_yn', 'attchmnt_for_od_yn',
+            'whatsapp_od_rqst_yn', 'teams_od_rqst_yn', 'onduty_week_off_emp_restricted_yn', 'onduty_holidays_emp_restricted_yn',
+            'from_days', 'to_days'
+        ];
+
+        foreach ($fieldsToDefaultZero as $field) {
+            if (!isset($data[$field]) || $data[$field] === null || $data[$field] === '') {
+                $data[$field] = 0;
+            }
+        }
+
+        $policy = CheckinPolicy::create($data);
 
         return response()->json([
             'status' => true,
@@ -67,7 +109,50 @@ class CheckinPolicyApiController extends Controller
             ], 404);
         }
 
-        $policy->update($request->all());
+        $data = $request->all();
+
+        // Fields that should default to "N/A" if empty
+        $fieldsToDefaultNA = [
+            'policy_name',
+            'applicability_type',
+            'applicability_for',
+            'advnc_applicability_type',
+            'advnc_applicability_for'
+        ];
+
+        foreach ($fieldsToDefaultNA as $field) {
+            if (isset($data[$field]) && ($data[$field] === null || $data[$field] === '')) {
+                $data[$field] = 'N/A';
+            }
+        }
+
+        // All other fields (except timestamps and id) default to 0 if empty
+        $fieldsToDefaultZero = [
+            'web_checkin_yn', 'punches_yn', 'punches_no', 'restrict_emp_marking_attndc_yn', 'punch_start_time', 'punch_end_time',
+            'IP_validation_yn', 'from_ip', 'to_ip', 'web_chckin_rqst_frm_whatsapp_yn', 'web_chckin_rqst_frm_teams_yn',
+            'mobile_chckin_yn', 'photo_attdnc_yn', 'no_of_photos', 'location_attdnc_yn', 'adv_location_tracking_yn',
+            'specific_period_wrk_location_approval_yn', 'location_approval_days_limit', 'max_punches_allowed_yn',
+            'punches_no_allowed', 'restrict_emp_attndc_yn', 'attdnc_regularization_yn', 'emp_bck_dated_attdnc_regularization_yn',
+            'emp_bck_dated_attdnc_regularization_days', 'mngr_bck_dated_regularization_yn', 'mngr_bck_dated_regularization_days',
+            'hr_bck_dated_attdnc_regularization_yn', 'hr_bck_dated_attdnc_regularization_days', 'attdnc_regularization_limit_type',
+            'attdnc_regularization_total_limit', 'ar_aftr_attdnc_process_yn', 'future_dtd_attdnc_regularization_yn',
+            'atleast_one_punch_attdnc_regularization_yn', 'for_ar_attachment_yn', 'whatsapp_ar_rqst_yn', 'teams_ar_rqst_yn',
+            'ar_week_off_emp_restricted_yn', 'ar_holidays_emp_restricted_yn', 'on_duty_yn', 'emp_bck_dtd_onduty_rqst_yn',
+            'emp_bck_dtd_onduty_rqst_days', 'mngr_bck_dtd_onduty_rqst_yn', 'mngr_bck_dtd_onduty_rqst_days',
+            'hr_bck_dtd_onduty_rqst_yn', 'hr_bck_dtd_onduty_rqst_days', 'configure_overall_onduty_limit_yn',
+            'project_log_time_yn', 'raise_onduty_aftr_attdnc_process_yn', 'future_dtd_onduty_yn',
+            'restrict_manager_onduty_beyond_limit_yn', 'restrict_hr_onduty_beyond_limit_yn', 'attchmnt_for_od_yn',
+            'whatsapp_od_rqst_yn', 'teams_od_rqst_yn', 'onduty_week_off_emp_restricted_yn', 'onduty_holidays_emp_restricted_yn',
+            'from_days', 'to_days'
+        ];
+
+        foreach ($fieldsToDefaultZero as $field) {
+            if (isset($data[$field]) && ($data[$field] === null || $data[$field] === '')) {
+                $data[$field] = 0;
+            }
+        }
+
+        $policy->update($data);
 
         return response()->json([
             'status' => true,
