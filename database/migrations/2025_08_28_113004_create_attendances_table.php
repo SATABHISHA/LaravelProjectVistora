4bca93b2e305ef1d45c8cca53818e5bf7fe80f95
ArbSoftware <satabhishar@arbsoft.comm>
2025-08-28 18:54:42 +0530

    modified

---
+++ database/migrations/2025_08_28_113004_create_attendances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->string('corpId');
+            $table->string('userName');
+            $table->string('empCode');
+            $table->string('companyName');
+            $table->string('checkIn')->nullable();
+            $table->string('checkOut')->nullable();
+            $table->string('Lat')->nullable();
+            $table->string('Long')->nullable();
+            $table->string('Address')->nullable();
+            $table->string('totalHrsForTheDay')->nullable();
+            $table->string('status')->nullable(); // Added status field
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
