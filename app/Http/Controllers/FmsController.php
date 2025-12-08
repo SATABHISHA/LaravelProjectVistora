f792b574a0d379f61388f8aecda9f1c0281fe2fe
SATABHISHA <satabhisha.innits@gmail.com>
2025-12-08 08:51:52 +0530

    Fix download URL generation - use url() instead of asset()

--- app/Http/Controllers/FmsController.php
+++ app/Http/Controllers/FmsController.php
@@ -200,7 +200,7 @@ class FmsController extends Controller
                 'fileType' => strtoupper($extension),
                 'empCode' => $file->empCode,
                 'file_size' => round($file->file_size / 1048576, 2) . ' MB',
-                'downloadUrl' => asset('storage/' . $file->file),
+                'downloadUrl' => url('storage/' . $file->file),
                 'uploaded_at' => $file->created_at,
             ];
         });
